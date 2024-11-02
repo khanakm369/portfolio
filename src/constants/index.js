150d38ea7a48f22dc1d302059bf33204edb1936d
KHANAK M <khanakm369@gmail.com>
2024-11-02 19:45:23 +0530

    video calling app

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ import project2 from "../assets/projects/project-2.webp";
 import project3 from "../assets/projects/project-3.webp";
 import project4 from "../assets/projects/project-4.webp";
 import project5 from "../assets/projects/project-5.webp.jpg"
+import project6 from "../assets/projects/project6.png"
 export const HERO_CONTENT = "I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1+ years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.";
 
 export const ABOUT_TEXT = "I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.";
@@ -12,50 +13,57 @@ export const EXPERIENCES = [
     year: "2023 - Present",
     role: "Software Engineer",
     company: "Autodesk",
-    description: "Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.",
+    description: "Played a key role in developing robust web applications with JavaScript, React.js, and Node.js, enhancing user experience and performance. Utilized MongoDB for efficient data storage solutions, implementing optimized schemas and query methods for large datasets. Worked closely with product managers to prioritize features and deliver enhancements, ensuring alignment with business goals.",
     technologies: ["React.js", "Node.js", "Express.js", "SQL Server", "C#", "ASP.NET MVC", "Next.js"],
   },
   {
     year: "2022 - Present",
     role: "Freelancer",
     company: "Self-Employed",
-    description: "Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.",
+    description: "Specialized in building and designing responsive user interfaces using Next.js and React, focusing on user-centric design. Coordinated with backend developers to seamlessly integrate frontend components with Node.js APIs. Implemented responsive designs and optimized loading times to enhance frontend performance, improving user engagement.",
     technologies: ["MERN Stack"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website Highly Scalable",
+    title: "E-Commerce Website – Highly Scalable",
     image: project1,
-    description: "Developed a feature-rich e-commerce platform designed to handle high traffic and scalability. Integrated functionalities such as dynamic product listing, a secure shopping cart, and user authentication. Focused on optimizing both frontend and backend performance to provide a seamless shopping experience. Implemented continuous integration and deployment (CI/CD) to ensure smooth updates and maintenance",
+    description: "Developed a feature-rich e-commerce platform capable of handling high traffic volumes, ensuring scalability and security. Integrated functionalities such as dynamic product listings, secure shopping cart, and user authentication. Focused on optimizing frontend and backend performance and set up CI/CD pipelines for seamless updates and maintenance.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB", "CI/CD"],
   },
   {
-    title: "Highly Scalable Video Streaming Website(NETFLIX Cloan)",
+    title: "Video Streaming Website (Netflix Clone)",
     image: project2,
-    description: "Created a high-performance video streaming website inspired by Netflix, focusing on scalability and user engagement. The platform supports user authentication, video playback, and personalized content recommendations. Implemented a responsive and intuitive UI using TailwindCSS and React. Integrated Firebase for real-time data management and leveraged continuous integration for automated testing and deployment.",
-    technologies: ["JSX", "TailwindCSS", "React", "API","Firebase , CI/CD"],
+    description: "Built a high-performance video streaming platform inspired by Netflix, emphasizing scalability and personalized recommendations. Included user authentication, video playback, and content recommendations to enhance user engagement. Used TailwindCSS and React to create a responsive, intuitive UI, integrated Firebase for real-time data handling, and CI/CD for automated testing and deployment.",
+    technologies: ["JSX", "TailwindCSS", "React", "API", "Firebase", "CI/CD"],
   },
   {
     title: "Portfolio Website",
     image: project3,
-    description: "Designed and developed a personal portfolio website to showcase professional skills, projects, and achievements. The site is fully responsive, optimized for performance, and deployed via Vercel for seamless updates. The project reflects modern web design principles and focuses on providing an engaging and intuitive user experience.",
+    description: "Designed a personal portfolio website to showcase professional skills, projects, and achievements. Built a fully responsive, high-performance site following modern web design principles and deployed it on Vercel for easy updates.",
     technologies: ["JSX", "TailwindCSS", "React", "Vercel"],
   },
   {
     title: "Blogging Platform",
     image: project4,
-    description: "Built a fully functional blogging platform where users can create, edit, and publish posts. The platform supports rich text editing, commenting, and user profiles. Implemented backend services for user authentication and data storage using MySQL, ensuring secure data management. Employed Vue.js for a dynamic and interactive frontend, while Express.js powers the backend.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    description: "Created a comprehensive blogging platform allowing users to write, edit, and publish content. Included rich text editing, comments, and user profiles. Secured backend services for authentication and data management using MySQL. Employed Vue.js for an interactive frontend and Express.js for backend services.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "MySQL"],
   },
   {
-    title: "Swiggy Cloan ",
+    title: "Swiggy Clone",
     image: project5,
-    description: " Developed a clone of Swiggy, focusing on the food delivery experience. This project includes functionalities such as restaurant listings, order management, and real-time tracking. Employed Vue.js and Express.js to create a responsive, fast, and user-friendly platform. MySQL is used for database management, ensuring scalability and efficient data storage",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    description: "Developed a Swiggy-inspired food delivery platform with restaurant listings, order management, and real-time tracking. Ensured a fast, responsive, and user-friendly platform using Vue.js for frontend and MySQL for scalable data storage.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "MySQL"],
+  },
+  {
+    title: "Video Calling App",
+    image: project6,
+    description: "Designed and built a portfolio site to demonstrate expertise, showcasing an optimized and engaging user experience. Fully responsive, optimized for performance, and deployed on Vercel.",
+    technologies: ["JSX", "TailwindCSS", "React", "Vercel" , "WebRTC" , "Zegocloud"],
   },
 ];
+
 
 export const CONTACT = {
   address: "Bangalore, India",
